0f84502d258a2b12780d535a7cb0254d5eb6e819
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-01-03 21:29:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,12 +5,12 @@ def initialize(name)
   @name = name
 @@all << self
 end
-def self.all
+def self.all # class method
 @@all.each do|dog| # iteration over the array @@all
 puts "#{dog.name}" # allows to print out the specific name of the dog >> Pluto, Fido, Maddi
   end
 end
-def self.clear_all
+def self.clear_all #class method
 @@all.clear
 end
 end
